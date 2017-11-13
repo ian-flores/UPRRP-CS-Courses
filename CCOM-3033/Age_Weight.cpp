814dadfa8cd13b75a629a8449680b3de1f367a74
ian-flores <iang9813@gmail.com>
2017-11-13 11:59:49 -0400

    Upoading CCOM-3033

---
+++ CCOM-3033/Age_Weight.cpp
@@ -0,0 +1,16 @@
+// Age and Weight Program 
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int age, weight;
+	
+	cout << "What is your weight? ";
+	cin >> weight;
+	cout << "What is your age? ";
+	cin >> age;
+	cout << "My age is " << age << " and my weight is " << weight << " pounds." << endl;
+
+    return 0; 
+} 
